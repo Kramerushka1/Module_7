c84d2394b7ac8ec41bdc436729b28355ad36b280
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-16 00:01:17 +0600

    Задание 7.1.10

--- Program.cs
+++ Program.cs
@@ -1,30 +1,43 @@
 ﻿namespace Module_7
 {
     /// <summary>
-    /// Задание 7.1.6
+    /// Задание 7.1.10
     /// </summary>
-    /// Реализуйте конструктор, заполняющий поля для следующего класса:
+    /// Для класса DerivedClass создайте 2 конструктора: 
+    ///     один, принимающий 2 параметра — name и description, 
+    ///     второй — принимающий 3 параметра name, description и counter.
+    ///Старайтесь написать оптимальный вариант с учетом полученных знаний.
 
-    internal class Program
+    class BaseClass
+    {
+        protected string Name;
+
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public string Description;
+        public int Counter;
+        
+        public DerivedClass(string name, string description) : base (name)
+        {
+            Description = description;
+        }
+        public DerivedClass(string name, string description, int counter) : this (name, description)
+        {
+            Counter = counter;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-    class Obj
-    {
-        private string name;
-        private string owner;
-        private int length;
-        private int count;
-
-        public Obj(string name, string ownerName, int objLength, int count)
-        {
-            this.name = name;
-            owner = ownerName;
-            length = objLength;
-            this.count = count;
+            
         }
     }
 
