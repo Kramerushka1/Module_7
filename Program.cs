ec2888306aa6eb940d7f9655472600570517dbd1
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-15 23:32:18 +0600

    Задание 7.1.5

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 ﻿namespace Module_7
 {
     /// <summary>
-    /// Задание 7.1.4
+    /// Задание 7.1.5
     /// </summary>
-    /// Для следующего класса Employee создайте 2 наследника: ProjectManager и Developer
-    /// Класс ProjectManager должен содержать строковое поле ProjectName, 
-    /// а класс Developer — строковое поле ProgrammingLanguage
+    /// Для следующего списка объектов создайте схему классов (объявите нужные классы и установите связи между ними):
+    ///     Apple(яблоко);
+    ///     Banana(банан);
+    ///     Pear(груша);
+    ///     Potato(картофель);
+    ///     Carrot(морковь).
+
     internal class Program
     {
         static void Main(string[] args)
@@ -13,18 +17,12 @@
             Console.WriteLine("Hello, World!");
         }
     }
-    class Employee
-    {
-        public string Name;
-        public int Age;
-        public int Salary;
-    }
-    class ProjectManager : Employee
-    {
-        public string ProjectName;
-    }
-    class Developer : Employee
-    {
-        private string ProgrammingLanguage;
-    }
+    class Food { }
+    class Fruit : Food { }
+    class Vegetable : Food { }
+    class Apple : Fruit { }
+    class Banana : Fruit { }
+    class Pear : Fruit { }
+    class Potato : Vegetable { }
+    class Carrot : Vegetable { }
 }
