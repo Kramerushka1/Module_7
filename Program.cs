aa95e557bb016a84524a7b72c009b698643bc504
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-16 10:35:48 +0600

    Задание 7.2.3

--- Program.cs
+++ Program.cs
@@ -1,44 +1,32 @@
 ﻿namespace Module_7
 {
     /// <summary>
-    /// Задание 7.1.10
+    /// Задание 7.2.3
     /// </summary>
-    /// Для класса DerivedClass создайте 2 конструктора: 
-    ///     один, принимающий 2 параметра — name и description, 
-    ///     второй — принимающий 3 параметра name, description и counter.
-    ///Старайтесь написать оптимальный вариант с учетом полученных знаний.
-
-    class BaseClass
-    {
-        protected string Name;
-
-        public BaseClass(string name)
-        {
-            Name = name;
-        }
-    }
-
-    class DerivedClass : BaseClass
-    {
-        public string Description;
-        public int Counter;
-        
-        public DerivedClass(string name, string description) : base (name)
-        {
-            Description = description;
-        }
-        public DerivedClass(string name, string description, int counter) : this (name, description)
-        {
-            Counter = counter;
-        }
-    }
+    /// Реализуйте в классе BaseClass виртуальный метод Display с типом void и без параметров, 
+    /// который будет выводить сообщение "Метод класса BaseClass" в консоль, 
+    /// а затем переопределите его в DerivedClass, 
+    /// чтобы он выводил сообщение "Метод класса DerivedClass".
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+           
         }
     }
-
+    class BaseClass
+    {
+        public virtual void Display()
+        {
+            Console.WriteLine("Метод класса BaseClass");
+        }
+    }
+    class DerivedClass : BaseClass
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Метод класса DerivedClass");
+        }
+    }
 }
