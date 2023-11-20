8431d972501b2512f2c0f5673ef017614e75561f
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-20 11:45:25 +0600

    Задание 7.5.5

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 namespace Module_7
 {
     /// <summary>
-    /// Задание 7.5.3
+    /// Задание 7.5.5
     /// </summary>    
     /// Создайте класс Helper и определите в нем статический метод Swap типа void, 
     /// который принимает 2 переменные типа int и меняет их значения местами.
@@ -14,20 +14,23 @@ namespace Module_7
     {
         static void Main(string[] args)
         {
-            int num1 = 3;
-            int num2 = 58;
-
-            Helper.Swap(ref num1, ref num2);
-
-            Console.WriteLine(num1); //58
-            Console.WriteLine(num2); //3
+            
         }
     }
-    class Helper
+    class Obj
     {
-        public static void Swap(ref int num1, ref int num2)
+        public string Name;
+        public string Description;
+
+        public static string Parent;
+        public static int DaysInWeek;
+        public static int MaxValue;
+
+        static Obj ()
         {
-            (num1, num2) = (num2, num1);
+            Parent = "System.Object";
+            DaysInWeek = 7;
+            MaxValue = 2000;
         }
     }
 }
