e947468b18923ce6fef0272e1c8bbe76c51a789b
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-20 12:06:51 +0600

    Задание 7.5.9

--- Program.cs
+++ Program.cs
@@ -3,34 +3,56 @@
 namespace Module_7
 {
     /// <summary>
-    /// Задание 7.5.5
+    /// Задание 7.5.9
     /// </summary>    
-    /// Создайте класс Helper и определите в нем статический метод Swap типа void, 
-    /// который принимает 2 переменные типа int и меняет их значения местами.
+    /// Для класса int создайте 2 метода расширения: GetNegative() и GetPositive().
+    /// Метод GetNegative должен возвращать отрицательное значение переменной(если оно положительно), либо саму переменную(если оно отрицательно или равно 0).
+    /// Метод GetPositive должен, наоборот, возвращать положительное значение(если оно отрицательно), либо саму переменную(если оно положительно или равно 0).
     /// Иными словами, для вашего метода должен будет корректно выполняться следующий код:
+
+
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int num1 = 7;
+            int num2 = -13;
+            int num3 = 0;
+
+            Console.WriteLine(num1.GetNegative()); //-7
+            Console.WriteLine(num1.GetPositive()); //7
+            Console.WriteLine(num2.GetNegative()); //-13
+            Console.WriteLine(num2.GetPositive()); //13
+            Console.WriteLine(num3.GetNegative()); //0
+            Console.WriteLine(num3.GetPositive()); //0
+
         }
     }
-    class Obj
+    static class IntExtensions
     {
-        public string Name;
-        public string Description;
-
-        public static string Parent;
-        public static int DaysInWeek;
-        public static int MaxValue;
-
-        static Obj ()
+        public static int GetNegative(this int value)
         {
-            Parent = "System.Object";
-            DaysInWeek = 7;
-            MaxValue = 2000;
+            if (value <= 0)
+            {
+                return value;
+            }
+            else
+            {
+                return 0 - value;
+            }
+        }
+        public static int GetPositive(this int value)
+        {
+            if (value >= 0)
+            {
+                return value;
+            }
+            else
+            {
+                return 0 - value;
+            }
         }
     }
 }
