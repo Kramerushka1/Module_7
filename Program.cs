60b416024f0d81a7c126cf18d85708e137a47545
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-20 11:16:10 +0600

    Задание 7.5.2

--- Program.cs
+++ Program.cs
@@ -1,48 +1,24 @@
-﻿namespace Module_7
+﻿using System.Threading.Channels;
+
+namespace Module_7
 {
     /// <summary>
-    /// Задание 7.2.4
-    /// </summary>
-    /// Измените свойство Counter так, чтобы его можно было переопределить в классе DerivedClass. 
-    /// Переопределите данное свойство, ограничив занесения в него чисел меньше 0.
-
-    class BaseClass
-    {
-        public virtual int Counter
-        {
-            get;
-            set;
-        }
-    }
-
-    class DerivedClass : BaseClass
-    {
-        private int counter;
-        public override int Counter
-        {
-            get
-            {
-                return counter;
-            }
-            set
-            {
-                if (value < 0)
-                {
-                    Console.WriteLine("Pанесения в него чисел меньше 0 невозможно");
-                }
-                else
-                {
-                    counter = value;
-                }
-            }
-        }
-    }
+    /// Задание 7.5.2
+    /// </summary>    
+    /// Создайте класс Obj, определите в нем поля Name, Description (тип строки) 
+    /// и статическое поле MaxValue типа int, равное 2000.
 
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             
         }
+    }
+    class Obj
+    {
+        public string Name;
+        public string Description;
+        public static int MaxValue = 2000;
     }
 }
