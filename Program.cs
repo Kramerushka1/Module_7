37ceeaa83e00d8aee4a058317c4e83d01b4a6c4e
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-11-20 11:25:56 +0600

    Задание 7.5.3

--- Program.cs
+++ Program.cs
@@ -3,22 +3,31 @@
 namespace Module_7
 {
     /// <summary>
-    /// Задание 7.5.2
+    /// Задание 7.5.3
     /// </summary>    
-    /// Создайте класс Obj, определите в нем поля Name, Description (тип строки) 
-    /// и статическое поле MaxValue типа int, равное 2000.
+    /// Создайте класс Helper и определите в нем статический метод Swap типа void, 
+    /// который принимает 2 переменные типа int и меняет их значения местами.
+    /// Иными словами, для вашего метода должен будет корректно выполняться следующий код:
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            int num1 = 3;
+            int num2 = 58;
+
+            Helper.Swap(ref num1, ref num2);
+
+            Console.WriteLine(num1); //58
+            Console.WriteLine(num2); //3
         }
     }
-    class Obj
+    class Helper
     {
-        public string Name;
-        public string Description;
-        public static int MaxValue = 2000;
+        public static void Swap(ref int num1, ref int num2)
+        {
+            (num1, num2) = (num2, num1);
+        }
     }
 }
