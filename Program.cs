1c6d32a9bddc257dfe4e79e314eb8623dac2d1ca
Kramerushka <email@example.rupainandlove95@gmail.com>
2023-12-12 23:50:50 +0600

    Модуль 7. Итоговое задание

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 
 namespace Module_7
@@ -11,83 +12,84 @@ namespace Module_7
     {
         static void Main(string[] args)
         {
+            Order<Product, HomeDelivery> order1 = new Order<Product, HomeDelivery>();
+            order1.Product = new Product { Name = "Шляпа", Count = 2, Price = 870f };
+            order1.Delivery = new HomeDelivery { Address = "ул. Хворостянского 9", deliverymanName = "Гриша" };
+            order1.DisplayProductInfo();
 
+            Order<Product, PickPointDelivery> order2 = new Order<Product, PickPointDelivery>();
+            order2.Product = new Product { Name = "Стул", Count = 3, Price = 1500f };
+            order2.Delivery = new PickPointDelivery { Address = "ул. Губернского 37", cellNumber = 10 };
+            order2.DisplayProductInfo();
+
+            Order<Product, ShopDelivery> order3 = new Order<Product, ShopDelivery>();
+            order3.Product = new Product { Name = "Торт", Count = 1, Price = 370f };
+            order3.Delivery = new ShopDelivery { Address = "ул. Героев Десантников 15", shopName = "Пятёрочка" };
+            order3.DisplayProductInfo();
         }
     }
-    class Order<TPerson, TOrderList> 
-        where TPerson : Person
-        where TOrderList : OrderList<Product>
+
+    class Order<TProduct, TDelivery>
+        where TProduct : Product
+        where TDelivery : Delivery
     {
-        private int orderNumber;
-        private TPerson? customer;
+        public int Number;
+        public TDelivery Delivery;
+        public TProduct Product;
 
-        public DateTime orderDate = DateTime.Now;
-
-        private TOrderList orderList;
-
-        public deliveryType;
-
-        private float totalPrice;
-
-        public void DisplayAddress()
+        public void DisplayProductInfo()
         {
-            Console.WriteLine(Delivery.address);
+            Console.WriteLine($"Товар: {Product.Name}, Количество: {Product.Count} шт.");
+            Console.WriteLine($"Адрес доставки: {Delivery.Address}");
+            Console.WriteLine($"Стоимость заказа: {Product.Price * Product.Count + Delivery.Price}");
+
+            if (Delivery is HomeDelivery homeDelivery)
+            {
+                Console.WriteLine($"Стоимость доставки: {homeDelivery.Price}");
+                Console.WriteLine($"Курьер: {homeDelivery.deliverymanName}");
+            }
+            else if (Delivery is PickPointDelivery pickPointDelivery)
+            {
+                Console.WriteLine($"Стоимость доставки: {pickPointDelivery.Price}");
+                Console.WriteLine($"Номер ячейки: {pickPointDelivery.cellNumber}");
+
+            }
+            else if (Delivery is ShopDelivery shopDelivery)
+            {
+                Console.WriteLine($"Стоимость доставки: {shopDelivery.Price}");
+                Console.WriteLine($"Магазин-получатель: {shopDelivery.shopName}");
+            }
+
+            Console.WriteLine($"Время доставки: {DateTime.Now.AddHours(1)}");
+            Console.WriteLine();
         }
     }
-    abstract class Customer
-    {
-        private string name;
-        private string phoneNumber;
-    }
-    class Person : Customer 
-    {
-        private bool hasPremiumService;
 
-        private string defaultHomeAddress;
-        private string defaultPickPointAddress;
-        private string defaultShopAddress;
-    }
-    class Organisation : Customer 
-    {
-        private string organisationAddress;
-        private bool hasPremiumService;
-    }
     class Product
     {
-        public string productName;
-        public float productPrice;
-        public string productDescription;
-        public bool isOnSale;
-        public bool hasDiscount;
+        public string? Name;
+        public int Count;
+        public float Price;
     }
-    class OrderList<TProduct> where TProduct : Product
-    {
-        public TProduct product;
-        private int productCount;
-        private float totalPrice;
 
-        public OrderList() 
-        {
-
-        }
-    }
-    class PremiumService { }
     abstract class Delivery
     {
-        public string address;
-        public float price;
+        public string? Address;
+        public float Price;
     }
-    class HomeDelivery : Delivery { }
-    class PickPointDelivery : Delivery { }
-    class ShopDelivery : Delivery { }
-    /*
-    class OrderNumberList
+    class HomeDelivery : Delivery
     {
-        private static int orderNumber;
-        private static int NewOrder()
-        {
-            return ++orderNumber;
-        }
+        public new float Price = 1000f;
+        public string? deliverymanName;
     }
-    */
+    class PickPointDelivery : Delivery
+    {
+        public new float Price = 500f;
+        public int cellNumber;
+    }
+    class ShopDelivery : Delivery
+    {
+        public new float Price = 200f;
+        public string? shopName;
+    }
 }
